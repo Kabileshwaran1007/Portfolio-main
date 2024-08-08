2ab619f56f887c7776e74c5bdcbce02fd683c3d1
Kabileshwaran M <kabileshwaranmuthiah1007@gmail.com>
2024-08-08 13:20:25 +0530

    updata code

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,34 +136,7 @@ fetchData().then(data => {
     showSkills(data);
 });
 
-// fetchData("projects").then(data => {
-//     showProjects(data);
-// });
 
-// <!-- tilt js effect starts -->
-// VanillaTilt.init(document.querySelectorAll(".tilt"), {
-//     max: 15,
-// });
-
-// document.onkeydown = function (e) {
-//     if (e.keyCode == 123) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//         return false;
-//     }
-// }
-
-/* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
     distance: '80px',
